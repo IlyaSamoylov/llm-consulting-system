7b62401edbf4374d9fd8058f257c0e2ca0538a0f
IlyaSamoylov <ilya.samoylov2004@mail.ru>
2026-06-05 17:34:57 +0500

    Add tests base structure, add unit tests for JWT and security

---
+++ auth_service/tests/unit/test_auth_security.py
@@ -0,0 +1,31 @@
+import pytest
+
+from app.core.security import (
+    hash_password,
+    verify_password,
+    create_access_token,
+    decode_access_token
+)
+
+@pytest.mark.unit
+def test_hash_password_not_equal_to_plain_password():
+    password = "StrongPassword123!"
+
+    hashed = hash_password(password)
+
+    assert hashed != password
+    assert verify_password(password, hashed) is True
+    assert verify_password("wrong-password", hashed) is False
+
+@pytest.mark.unit
+def test_create_and_decode_access_token():
+    payload_in = {"sub": "123", "role": "admin"}
+
+    token = create_access_token(**payload_in)
+    payload_out = decode_access_token(token)
+
+    assert payload_out["sub"] == "123"
+    assert payload_out["role"] == "admin"
+    assert "iat" in payload_out
+    assert "exp" in payload_out
+    assert payload_out["exp"] > payload_out["iat"]
