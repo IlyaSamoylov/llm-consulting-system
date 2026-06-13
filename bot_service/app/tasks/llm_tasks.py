0d0dba5f0a2a4cea6c90ccd078dad5682950bac0
IlyaSamoylov <ilya.samoylov2004@mail.ru>
2026-06-13 17:10:38 +0500

    Add OpenRouter client, integrate into llm-tasks

--- bot_service/app/tasks/llm_tasks.py
+++ bot_service/app/tasks/llm_tasks.py
@@ -3,35 +3,25 @@ import httpx
 
 from app.core.config import settings
 from app.infra.celery_app import celery_app
+from app.services.openrouter_client import call_openrouter, OpenRouterError
 
-# TODO: наверное, полезно попробовать использовать logger еще в других местах и в auth-service тоже
 logger = logging.getLogger(__name__)
 
-# TODO: Заменить на реальный ответ из openrouter_client
-def call_openrouter_sync(prompt) -> str:
-	"""Заглушка вместо ответа от OpenRouter"""
-	return "Какой-то ответ LLM"
-
-class OpenrouterError(RuntimeError):
-	pass
-
 @celery_app.task(name="app.tasks.llm_tasks.llm_request")
-def llm_request(tg_chat_id: int, prompt: str) -> str:
+def llm_request(tg_chat_id: int, prompt: str) -> str | None:
+	"""Celery-задача, получает ответ от OpenRouter, формирует ответ и отправляет сообщение пользователю"""
+
 	try:
-		llm_answer = call_openrouter_sync(prompt)
-	except OpenrouterError as exc:
+		llm_answer = call_openrouter(prompt)
+	except OpenRouterError as exc:
 		llm_answer = f"Ошибка LLM: {exc}"
 
-	_send_telegram_message(tg_chat_id=tg_chat_id, text=llm_answer)
-	return llm_answer
-
-def _send_telegram_message(*, tg_chat_id: int, text: str):
 	if not settings.TELEGRAM_BOT_TOKEN:
 		logger.warning("Токен бота телеграм не задан, пропускаю")
 		return
 
 	url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
-	payload = {"chat_id": tg_chat_id, "text": text}
+	payload = {"chat_id": tg_chat_id, "text": llm_answer}
 
 	try:
 		with httpx.Client(timeout=20.0) as client:
@@ -39,3 +29,5 @@ def _send_telegram_message(*, tg_chat_id: int, text: str):
 			response.raise_for_status()
 	except httpx.HTTPError:
 		logger.exception("Не удалось отправить сообщение из Celery worker")
+
+	return llm_answer
